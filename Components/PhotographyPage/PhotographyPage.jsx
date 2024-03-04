6b42bd3ba4c4944ea929a05ddc62ed94c46e887b
Zac Edens <edens.zac@gmail.com>
2024-03-03 19:45:13 -0800

    New update, added short term metadata for all images in project.

--- Components/PhotographyPage/PhotographyPage.jsx
+++ Components/PhotographyPage/PhotographyPage.jsx
@@ -4,9 +4,19 @@ import ParallaxSection from "../ParallaxSection/ParallaxSection";
 export default function PhotographyPage( { projectStructure } ) {
     return (
         <div className={styles.bodyWrapper}>
+            {/* TODO: Additional Elements go in here. Do we make them PART of our .map below? for Dynamic organization...*/}
+            {/*<div className={styles.photoHeader}>*/}
+            {/*    <h1 className={styles.photoHeaderTitle}>Zac</h1>*/}
+            {/*</div>*/}
             {projectStructure.map( ( imageLocation, index ) => (
                 <ParallaxSection key={index} title={imageLocation.title} bannerImage={imageLocation.bannerImage}/>
             ) )}
+            <div className={styles.footer}>
+                <a className={styles.title}>edens.zac production</a>
+                <a>instagram</a>
+                <a>email</a>
+                <a></a>
+            </div>
         </div>
     )
 };
