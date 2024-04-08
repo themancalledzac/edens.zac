53c23a45f0575bc69b8ef3d9f9329d35a853e8f0
Zac Edens <edens.zac@gmail.com>
2024-04-07 18:32:24 -0700

    Image full screen zoom in and out, including an escape button exit is now working. NextJS Image component takes up full screen, however, so can't 'click outside of the image to close'.

--- Components/ImageFullScreen/ImageFullScreen.jsx
+++ Components/ImageFullScreen/ImageFullScreen.jsx
@@ -1,24 +1,59 @@
 import Image from "next/image";
 import styles from './ImageFullScreen.module.scss';
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const isValidSource = ( title ) => {
     return title && title !== "";
 };
 
 export default function ImageFullScreen( { imageSelected, setImageSelected } ) {
+    const [isZoomed, setIsZoomed] = useState( false );
+
+    useEffect( () => {
+        const handleKeyDown = ( event ) => {
+            if ( event.key === "Escape" ) {
+                setImageSelected( null );
+            }
+        };
+
+        window.addEventListener( "keydown", handleKeyDown );
+
+        // Cleanup
+        return () => window.removeEventListener( "keydown", handleKeyDown );
+    }, [setImageSelected] );
+
+    // Not yet working as NextJS Image is taking 100% of screen. investigating
+    const handleClickOutside = () => {
+        if ( isZoomed ) {
+            setIsZoomed( false );
+        } else {
+            setImageSelected( null );
+        }
+    };
+
+    const handleImageClick = ( e ) => {
+        e.stopPropagation(); // prevent triggering handleClickOutside
+        setIsZoomed( !isZoomed );
+    }
+
     const handleClick = () => {
         setImageSelected( null );
     }
+    console.log( 'isZoomed: ' + isZoomed );
 
     return (
-        <div className={styles.imageFullScreenWrapper} onClick={handleClick}>
+        <div className={styles.imageFullScreenWrapper} onClick={handleClickOutside}>
             <Image
                 src={isValidSource( imageSelected?.title ) ? `/${imageSelected.title}` : ""}
                 alt={'Photo'}
                 layout="fill"
-                objectFit="contain"
+                objectFit={isZoomed ? "cover" : "contain"}
                 priority={true}
+                style={{
+                    backgroundColor: "rgba(17, 17, 17, 0.75)",
+                    cursor: isZoomed ? "move" : "default",
+                }}
+                onClick={handleImageClick}
             />
         </div>
     )
