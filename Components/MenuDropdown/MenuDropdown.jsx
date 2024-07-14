12548290567c16126234318453ea4e11dd85d43a
Zac Edens <edens.zac@gmail.com>
2024-07-14 14:29:06 -0700

    updated styling location for dropdown, also fixed issue with dropdown buttons and selected dropdown styling being similar but breaking.

--- Components/MenuDropdown/MenuDropdown.jsx
+++ Components/MenuDropdown/MenuDropdown.jsx
@@ -1,9 +1,11 @@
-import styles from "../Header/Header.module.scss";
-import { CircleX, Instagram } from "lucide-react";
+import styles from "./MenuDropdown.module.scss";
+import { CircleX, Undo2 } from "lucide-react";
 import { useState } from "react";
+import InstagramIcon from "../InstagramIcon/InstagramIcon";
 
 export default function MenuDropdown( { dropdownRef, showDropdown, setShowDropdown } ) {
     const [aboutDropdownVisible, setAboutDropdownVisible] = useState( false );
+    const [contactDropdownVisible, setContactDropdownVisible] = useState( false );
 
     return (
         <div className={styles.dropdown} ref={dropdownRef}>
@@ -14,21 +16,59 @@ export default function MenuDropdown( { dropdownRef, showDropdown, setShowDropdo
                     onClick={() => setShowDropdown( !showDropdown )}
                 />
             </div>
-            <div className={styles.dropdownMenuOptions}
-                 onClick={() => ( setAboutDropdownVisible( !aboutDropdownVisible ) )}>About
-            </div>
-            <div className={styles.dropdownMenuOptions}>Contact</div>
-            <div className={styles.dropdownMenuOptions}>
-                <Instagram onClick={() => window.location.href = 'https://instagram.com/themancalledzac'}/>
-            </div>
+            {aboutDropdownVisible ? (
+                <div className={styles.dropdownSubMenu}>
+                    <div className={styles.dropdownMenuOptionsWrapper}>
+                        <div className={styles.dropdownMenuOption}>
+                            <Undo2
+                                className={styles.dropdownBackButton}
+                                onClick={() => setAboutDropdownVisible( !aboutDropdownVisible )}
+                            />
+                            <h2>About</h2>
+                        </div>
+                        {/* Add your about content here */}
+                    </div>
+                </div>
+            ) : contactDropdownVisible ? (
+                <div className={styles.dropdownSubMenu}>
+                    <div className={styles.dropdownMenuOptionsWrapper}>
+                        <div className={styles.dropdownMenuOption}>
+                            <Undo2
+                                className={styles.dropdownBackButton}
+                                onClick={() => setContactDropdownVisible( !contactDropdownVisible )}
+                            />
+                            <h2>Contact</h2>
+                        </div>
+                        {/* Add your contact content here */}
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.dropdownMenuOptionsWrapper}>
+                    <div className={styles.dropdownMenuItem}>
+                        <h2 className={styles.dropdownMenuOptions}
+                            onClick={() => setAboutDropdownVisible( !aboutDropdownVisible )}>About</h2>
+                    </div>
+                    <div className={styles.dropdownMenuItem}>
+                        <h2 className={styles.dropdownMenuOptions}
+                            onClick={() => setContactDropdownVisible( !contactDropdownVisible )}>Contact</h2>
+                    </div>
+                    <div className={`${styles.dropdownMenuItem} ${styles.dropdownMenuOptions}`}>
+                        <InstagramIcon
+                            size={32}
+                            onClick={() =>
+                                window.open( 'https://instagram.com/themancalledzac', '_blank', 'noopener,noreferrer' )}
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
 
 // TODO:
-//  1. About Dropdown
-//   - OnClick => open AboutContainer
-//   - OpenContainer should take over entire space of Dropdown
-//   -
-//   -
-//  2. Contact Dropdown
+// 1. About Dropdown
+// - OnClick => open AboutContainer
+// - OpenContainer should take over entire space of Dropdown
+// -
+// -
+// 2. Contact Dropdown
